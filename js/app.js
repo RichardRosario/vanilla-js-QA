59eb08c6b2a335ee7b0427eae37462b50fbb7098
Richard Rosario <tech2js@gmail.com>
2020-06-23 21:50:20 +0800

    working the functionality

--- js/app.js
+++ js/app.js
@@ -8,3 +8,92 @@
 // const questionList = document.getElementById("questions-list");
 // let data = [];
 // let id = 1;
+
+// event listeners
+
+function eventListeners() {
+  const showBtn = document.getElementById("show-btn");
+  const questionCard = document.querySelector(".question-card");
+  const closeBtn = document.querySelector(".close-btn");
+  const form = document.getElementById("question-form");
+  const feedback = document.querySelector(".feedback");
+  const questionInput = document.getElementById("question-input");
+  const answerInput = document.getElementById("answer-input");
+  const questionList = document.getElementById("question-list");
+
+  let data = [];
+  let id = 1;
+
+  // new UI instance
+  const ui = new UI();
+  // show question form
+  showBtn.addEventListener("click", function () {
+    ui.showQuestion(questionCard);
+  });
+  // hide question form
+  closeBtn.addEventListener("click", function () {
+    ui.hideQuestion(questionCard);
+  });
+  // add question
+  form.addEventListener("click", function (event) {
+    event.preventDefault();
+    const questionValue = questionInput.value;
+    const answerValue = answerInput.value;
+    if (questionValue === "" || answerValue === "") {
+      feedback.classList.add("showItem", "alert-danger");
+      feedback.textContent = "Cannot add empty values";
+      setTimeout(function () {
+        feedback.classList.remove("showItem", "alert-danger");
+      }, 4000);
+    } else {
+      const question = new Question(id, questionValue, answerValue);
+      data.push(question);
+      id++;
+      ui.addQuestions(questionList, question);
+      ui.clearFields(questionInput, answerInput);
+    }
+  });
+}
+// UI construtor
+function UI() {}
+
+// show question card
+UI.prototype.showQuestion = function (element) {
+  element.classList.add("showItem");
+};
+// hide question card
+UI.prototype.hideQuestion = function (element) {
+  element.classList.remove("showItem");
+};
+// add questions
+UI.prototype.addQuestions = function (element, question) {
+  const div = document.createElement("div");
+  div.classList.add("col-md-4");
+  div.innerHTML = ` <div class="card card-body flashcard my-3">
+     <h4 class="text-capitalize">${question.title}?</h4>
+     <a href="#" class="text-capitalize my-3 show-answer">show/hide answer</a>
+     <h5 class="answer mb-3">${question.answer}</h5>
+     <div class="flashcard-btn d-flex justify-content-between">
+
+      <a href="#" id="edit-flashcard" class=" btn my-1 edit-flashcard text-uppercase" data-id="${question.id}">edit</a>
+      <a href="#" id="delete-flashcard" class=" btn my-1 delete-flashcard text-uppercase">delete</a>
+     </div>
+     </div>`;
+};
+// clear fields
+UI.prototype.clearFields = function (question, answer) {
+  question.value = "";
+  answer.value = "";
+};
+
+// question constructor
+function Question(id, title, answer) {
+  this.id = id;
+  this.title = title;
+  this.answer = answer;
+}
+
+// dom event listener
+document.addEventListener("DOMContentLoaded", function () {
+  eventListeners();
+});
